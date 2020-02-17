86627840117c90aa56129ff818195cf0c9a1be78
Martin Schmiele <martins@sb3.hco.nbi.dk>
2020-02-17 18:21:54 +0100

    updated BMFS

--- run_BatchMultiFitStat_20190121_1to5dil.m
+++ run_BatchMultiFitStat_20190121_1to5dil.m
@@ -60,11 +60,18 @@ dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_*00*","",Inf
 
 (*
 Nsp=6;
+
+(* 4:4:96 -> 300 *)
 mindisl=4;maxdisl=100;ddisl=4;
 mindosl=4;maxdosl=100;ddosl=4;
 
 (* SSS_*BC_1to5dil_OSL_ASSYM_005, SSS_*BC_1to5dil_OSL_HUGE_005_more, SSS_*BC_1to5dil_OSL_LARGER_005_more and SSS_*BC_1to5dil_OSL_SMALLER_005_more *)
 dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_*00*","",Infinity],StringMatchQ[#,{_~~"*005_more"}]&],DirectoryQ];
+dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_ASSYM_00*","",Infinity],StringMatchQ[#,{_~~"*005"}]&],DirectoryQ];
+
+(* 8:4:92 -> 253 *)
+mindisl=8;maxdisl=92;ddisl=4;
+mindosl=8;maxdosl=92;ddosl=4;
 dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_ASSYM_00*","",Infinity],StringMatchQ[#,{_~~"*005"}]&],DirectoryQ];
 *)
 
@@ -91,22 +98,37 @@ dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_0*","",Infin
 
 (* simulation params for cis, disl and dosl VOSL symmshells *)
 
-Nsp=6;
 (*
+Nsp=6;
 mindisl=4;maxdisl=100;ddisl=4;
 mindosl=4;maxdosl=100;ddosl=4;
 dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_ASSYM_symmshells_0*","",Infinity],StringMatchQ[#,{_~~"*005"}]&],DirectoryQ];
 *)
 
-mindisl=8;maxdisl=92;ddisl=4;
-mindosl=8;maxdosl=92;ddosl=4;
-dirlist=Select[Select[FileNames["MathematicaOut/SSS_*BC_1to5dil_OSL_ASSYM_00*","",Infinity],StringMatchQ[#,{_~~"*005"}]&],DirectoryQ];
 
 
 
+(*
+native samples *011_v2* *013_v2*
+0p0		disl=8:4:32, dosl=8:4:32 with 24<=dtot<=48
+3p0,5p0,7p5	disl=8:4:20, dosl=20:4:52 with 24<=dtot<=60
+*)
 
+(*
+Nsp=6;Nst=3
+mindisl=8;maxdisl=32;ddisl=4;
+mindosl=8;maxdosl=32;ddosl=4;
+dirlist=Select[Select[FileNames["MathematicaOut/SSS_0p0BC_native_STMOD3_nis_totd_OSL_0*","",Infinity],StringMatchQ[#,{_~~"*011_v2",_~~"*013_v2"}]&],DirectoryQ];
+*)
 
-
+Nsp=6;Nst=3
+mindisl=8;maxdisl=20;ddisl=4;
+mindosl=20;maxdosl=52;ddosl=4;
+(*
+dirlist=Select[Select[FileNames["MathematicaOut/SSS_3p0BC_native_STMOD3_nis_totd_OSL_0*","",Infinity],StringMatchQ[#,{_~~"*011_v2",_~~"*013_v2"}]&],DirectoryQ];
+dirlist=Select[Select[FileNames["MathematicaOut/SSS_5p0BC_native_STMOD3_nis_totd_OSL_0*","",Infinity],StringMatchQ[#,{_~~"*011_v2",_~~"*013_v2"}]&],DirectoryQ];
+*)
+dirlist=Select[Select[FileNames["MathematicaOut/SSS_7p5BC_native_STMOD3_nis_totd_OSL_0*","",Infinity],StringMatchQ[#,{_~~"*011_v2*",_~~"*013_v2*"}]&],DirectoryQ];
 
 
 Print[dirlist];
@@ -136,17 +158,25 @@ files=SelectFilesdisldosl[files,mindisl,maxdisl,mindosl,maxdosl];
 
 (* derive default fit params from log-files, disl and dosl must not be passed to DeriveTp, choices for target fnc value: "Target function values", "Chi2Red function values", "LogdI function values" *)
 
+(*
+if a param was fixed and the new BMF version was used in the fitting, it will find and process the fixed values as well, for old fits, exclude, but will crash at some point ...
+lablist=Join[{"filename","disl","dosl","Target function values"},Table["c"<>ToString[i],{i,1,Nsp}],{"rhoisl","rhoosl","chiXn1"}];
+*)
 
+
+(* 
+(* dil *)
 lablist=Join[{"filename","disl","dosl","Target function values"},Table["c"<>ToString[i],{i,1,Nsp}],{"rhoisl","rhoosl","rhodm","chiXn1"}];
+*)
+
+(* nat *)
+lablist=Join[{"filename","disl","dosl","Target function values"},Table["c"<>ToString[i],{i,1,Nsp}],{"rhoisl","rhoosl","rhodm","chiXn1","chi"},Table["d"<>ToString[i],{i,2,Nst}]];
+
 
 (*
 lablist=Join[{"filename","disl","dosl","Chi2Red function values"},Table["c"<>ToString[i],{i,1,Nsp}],{"rhoisl","rhoosl","rhodm","chiXn1"}];
 *)
 
-(*
-if a param was fixed and the new BMF version was used in the fitting, it will find and process the fixed values as well, for old fits, exclude, but will crash at some point ...
-lablist=Join[{"filename","disl","dosl","Target function values"},Table["c"<>ToString[i],{i,1,Nsp}],{"rhoisl","rhoosl","chiXn1"}];
-*)
 
 
 Print[lablist];
@@ -156,7 +186,7 @@ Tpdata=DeriveTp[files,lablist[[4;;]]];
 AppendTo[lablist,"dtot"];
 Do[AppendTo[Tpdata[[i]],Tpdata[[i,1+1]]+Tpdata[[i,1+2]]],{i,1,Length[files]}];
 
-(* ici, incorrect *)
+(* ini, simply takes the cis (incl shells) and averages over i *)
 AppendTo[lablist,"ici"];
 Do[AppendTo[Tpdata[[i]],Sum[Tpdata[[i,4+j]]*j,{j,1,Nsp}]/Sum[Tpdata[[i,4+j]],{j,1,Nsp}]],{i,1,Length[files]}];
 
@@ -171,7 +201,7 @@ AppendTo[lablist,"n"<>ToString[j]];
 Do[AppendTo[Tpdata[[i]],(Tpdata[[i,4+j]]/(j*d001+2*(Tpdata[[i,1+1]]+Tpdata[[i,1+2]])))/Sum[Tpdata[[i,4+k]]/(k*d001+2*(Tpdata[[i,1+1]]+Tpdata[[i,1+2]])),{k,1,Nsp}]],{i,1,Length[files]}];
 ,{j,1,Nsp}]
 
-(* ini, incorrect *)
+(* ini, simply takes the nis (incl shells) and averages over i *)
 AppendTo[lablist,"ini"];
 Do[AppendTo[Tpdata[[i]],Sum[j*(Tpdata[[i,4+j]]/(j*d001+2*(Tpdata[[i,1+1]]+Tpdata[[i,1+2]])))/Sum[Tpdata[[i,4+k]]/(k*d001+2*(Tpdata[[i,1+1]]+Tpdata[[i,1+2]])),{k,1,Nsp}],{j,1,Nsp}]],{i,1,Length[files]}];
 
